c69862ca0b1cf01e25c1ed038819881182aef3b3
Yuri Ivanov <y.ivanov@inostudio.com>
2018-10-11 11:35:50 +0300

    Blank project for second chapter's exercises + BMI calculator

---
+++ ch02/ex32/bmi_calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double getWeightInKillograms(void) {
+    double weight = 0.0;
+
+    printf("Enter your weight in kilograms: ");
+    scanf("%lf", &weight);
+
+    return weight;
+}
+
+double getHeightInMeters(void) {
+    double height = 0.0;
+
+    printf("Enter your height in meters: ");
+    scanf("%lf", &height);
+
+    return height;
+}
+
+double calculateBmi(const double weightInKillograms, const double heightInMeters) {
+    return weightInKillograms / (heightInMeters * heightInMeters);
+}
+
+void printBmi(const double bmi) {
+    printf("\nYour BMI is: %.2lf\n", bmi);
+    puts(
+        "BMI VALUES\n"
+        "Underweight: less than 18.5\n"
+        "Normal:      between 18.5 and 24.9\n"
+        "Overweight:  between 25 and 29.9\n"
+        "Obese:       30 or greater");
+}
+
+int main(void) {
+    double weight = getWeightInKillograms();
+    double height = getHeightInMeters();
+    double bmi = calculateBmi(weight, height);
+
+    printBmi(bmi);
+}
