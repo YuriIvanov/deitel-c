12ce76a7bfd039197c394760aef2a59ebb93d0d1
Yuri Ivanov <y.ivanov@inostudio.com>
2018-10-12 11:45:42 +0300

    Chapter 2, exercise 26 written.

--- ch02/ex26/multiples.c
+++ ch02/ex26/multiples.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 
 int main(void) {
+    int a = 0;
+    int b = 0;
+
+    printf("%s", "Enter two integer numbers: ");
+    scanf("%d%d", &a, &b);
+
+    if (!(b % a)) {
+        printf("%d is multiple of %d\n", b, a);
+    } else {
+        printf("%d is not multiple of %d\n", b, a);
+    }
 
     return EXIT_SUCCESS;
 }
