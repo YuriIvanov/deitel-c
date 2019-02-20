a3f079fcda35e49487cc98be7c8cfeeafd00cfd5
Yuri Ivanov <y.ivanov@inostudio.com>
2019-02-20 11:00:34 +0300

    Chapter 4, ex33.

--- ch04/ex33/roman_numeral/roman_numeral.c
+++ ch04/ex33/roman_numeral/roman_numeral.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// See https://www.rapidtables.com/convert/number/how-number-to-roman-numerals.html.
+static const int DECIMAL_TABLE[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+static const char *ROMAN_SYMBOL_TABLE[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+static void decimalToRomanNumeral(const int number) {
+    if (number <= 0) {
+        puts("Romans had no zero of negative numbers.");
+        exit(-1);
+    }
+
+    int decimal = number;
+    printf("%-7d ", decimal);
+
+    int i = 0;
+    while(decimal) {
+        while(decimal / DECIMAL_TABLE[i]) {
+            printf("%s", ROMAN_SYMBOL_TABLE[i]);
+            decimal -= DECIMAL_TABLE[i];
+        }
+        i++;
+    }
+    puts("");
+}
+
 int main(void) {
+    printf("%-7s %-5s\n", "Decimal", "Roman");
+    for (int decimal = 1; decimal <= 100; ++decimal) {
+        decimalToRomanNumeral(decimal);
+    }
+
     return EXIT_SUCCESS;
 }
