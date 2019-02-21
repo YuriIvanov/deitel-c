18d4a8818c7ef63eca4948c743503c5fdfde7308
Yuri Ivanov <y.ivanov@inostudio.com>
2019-02-21 15:38:29 +0300

    Chapter 4, ex38 and ex39.

--- ch04/ex38/twelve_days_of_christmas/twelve_days_of_christmas.c
+++ ch04/ex38/twelve_days_of_christmas/twelve_days_of_christmas.c
@@ -1,6 +1,168 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_DAYS 12
+
+static const char* DAYS_TO_STRINGS[MAX_DAYS + 1] = {
+    "",
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+};
+
+static const char FIRST_DAY_GIFTS[] =
+    "A partridge in a pear tree";
+
+static const char SECOND_DAY_GIFTS[] =
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char THIRD_DAY_GIFTS[] =
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char FOURTH_DAY_GIFTS[] =
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char FIFTH_DAY_GIFTS[] =
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char SIXTH_DAY_GIFTS[] =
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char SEVENTH_DAY_GIFTS[] =
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char EIGTH_DAY_GIFTS[] =
+    "Eight maids a-milking\n"
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char NINTH_DAY_GIFTS[] =
+    "Nine ladies dancing\n"
+    "Eight maids a-milking\n"
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char TENTH_DAY_GIFTS[] =
+    "Ten lords a-leaping\n"
+    "Nine ladies dancing\n"
+    "Eight maids a-milking\n"
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char ELEVENTH_DAY_GIFTS[] =
+    "Eleven pipers piping\n"
+    "Ten lords a-leaping\n"
+    "Nine ladies dancing\n"
+    "Eight maids a-milking\n"
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char TWELFTH_DAY_GIFTS[] =
+    "Twelve drummers drumming\n"
+    "Eleven pipers piping\n"
+    "Ten lords a-leaping\n"
+    "Nine ladies dancing\n"
+    "Eight maids a-milking\n"
+    "Seven swans a-swimming\n"
+    "Six geese a-laying\n"
+    "Five golden rings\n"
+    "Four calling birds\n"
+    "Three french hens\n"
+    "Two turtle doves, and\n"
+    "A partridge in a pear tree";
+
+static const char* GIFTS_BY_DAYS[MAX_DAYS + 1] = {
+    NULL,
+    FIRST_DAY_GIFTS,
+    SECOND_DAY_GIFTS,
+    THIRD_DAY_GIFTS,
+    FOURTH_DAY_GIFTS,
+    FIFTH_DAY_GIFTS,
+    SIXTH_DAY_GIFTS,
+    SEVENTH_DAY_GIFTS,
+    EIGTH_DAY_GIFTS,
+    NINTH_DAY_GIFTS,
+    TENTH_DAY_GIFTS,
+    ELEVENTH_DAY_GIFTS,
+    TWELFTH_DAY_GIFTS
+};
+
+static void assertDay(const int day) {
+    if (day < 1 || day > MAX_DAYS) {
+        puts("Day should be between 1 and 12.");
+        exit(-1);
+    }
+}
+
+static const char* const dayToString(const int day) {
+    assertDay(day);
+    return DAYS_TO_STRINGS[day];
+}
+
+static const char* const getGiftsByDay(const int day) {
+    assertDay(day);
+    return GIFTS_BY_DAYS[day];
+}
+
 int main(void) {
+    for (int day = 1; day <= 12; ++day) {
+        printf("On the %s day of Christmas\n"
+               "my true love sent to me:\n"
+               "%s\n",
+               dayToString(day),
+               getGiftsByDay(day));
+        puts("");
+    }
+
     return EXIT_SUCCESS;
 }
--- ch04/ex39/floating_point_limitations/floating_point_limitations.c
+++ ch04/ex39/floating_point_limitations/floating_point_limitations.c
@@ -2,5 +2,10 @@
 #include <stdlib.h>
 
 int main(void) {
+    float money = 1000000.00f;
+    money += 0.12f;
+
+    printf("Your money: %.2f", money);
+
     return EXIT_SUCCESS;
 }
