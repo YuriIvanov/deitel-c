238a13c462cef246200442ae066a46de6ba70174
Yuri Ivanov <y.ivanov@inostudio.com>
2019-04-29 09:30:14 +0300

    Chapter 5, ex11 and ex13.

--- ch05/ex11/rounding_numbers_floor.c
+++ ch05/ex11/rounding_numbers_floor.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+static double roundToInteger(const double number) {
+    return floor(number+ 0.5);
+}
+
+static double roundToTenths(const double number) {
+    return floor(number * 10 + 0.5) / 10;
+}
+
+static double roundToHundreths(const double number) {
+    return floor(number * 100 + 0.5) / 100;
+}
+
+static double roundToThousandths(const double number) {
+    return floor(number * 1000 + 0.5) / 1000;
+}
+
+static double getNumberToRound() {
+    double numberToRound = 0.0;
+
+    int scanfResult = -1;
+    do {
+        printf("%s", "Enter number to round (Ctrl+C to exit): ");
+
+        scanfResult = scanf("%lf", &numberToRound);
+        if (scanfResult != 1) {
+            fflush(stdin);
+            puts("You've entered incorrect number to round. Try again.");
+        }
+    } while (scanfResult != 1);
+
+    return numberToRound;
+}
 
 int main(void) {
+    while (true) {
+        const double numberToRound = getNumberToRound();
+        printf(
+            "\n%s: %.6lf"
+            "\n%s: %.0lf"
+            "\n%s: %.1lf"
+            "\n%s: %.2lf"
+            "\n%s: %.3lf\n\n",
+            "Original number", numberToRound,
+            "Rounded to integer", roundToInteger(numberToRound),
+            "Rounded to tenths", roundToTenths(numberToRound),
+            "Rounded to hundredths", roundToHundreths(numberToRound),
+            "Rounded to thousandths", roundToThousandths(numberToRound)
+        );
+    }
+
     return EXIT_SUCCESS;
 }
--- ch05/ex13/random_integers.c
+++ ch05/ex13/random_integers.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <stdbool.h>
+
+#define MAX_RANDOM_NUMBERS 10
+
+static void initRandom(void) {
+    srand(time(NULL));
+}
 
 int main(void) {
+    initRandom();
+
+    printf("%s", "Random integers in range 1 <= n <= 6:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", 1 + rand() % 6);
+    }
+
+    printf("%s", "\nRandom integers in range 100 <= n <= 1000:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", 100 + rand() % 901);
+    }
+
+    printf("%s", "\nRandom integers in range 0 <= n <= 19:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", rand() % 20);
+    }
+
+    printf("%s", "\nRandom integers in range 1000 <= n <= 2222:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", 1000 + rand() % 1223);
+    }
+
+    printf("%s", "\nRandom integers in range -1 <= n <= 1:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", -1 + rand() % 3);
+    }
+
+    printf("%s", "\nRandom integers in range -3 <= n <= 11:\n");
+    for (int i = 0; i < MAX_RANDOM_NUMBERS; ++i) {
+        printf("%d\n", -3 + rand() % 15);
+    }
+
     return EXIT_SUCCESS;
 }
