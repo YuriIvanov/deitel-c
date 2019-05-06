6bac508e07635c54c48e08c19f3417d8bd2c0af9
Yuri Ivanov <y.ivanov@inostudio.com>
2019-05-06 14:50:23 +0300

    Chapter 5, ex23 and ex24.

--- ch05/ex23/time_in_seconds.c
+++ ch05/ex23/time_in_seconds.c
@@ -1,6 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+static int getTwelveHourClockHours(void) {
+    int hours = -1;
+
+    bool isUserInputValid = false;
+    while (!isUserInputValid) {
+        printf("%s", "Enter twelve hour clock hours (1-12): ");
+        const int scanfResult = scanf("%d", &hours);
+
+        isUserInputValid =
+            scanfResult == 1 &&
+            hours >= 1 && hours <= 12;
+
+        if (!isUserInputValid) {
+            fflush(stdin);
+            puts("You've entered incorrect hours. Try again.");
+        }
+    };
+
+    return hours;
+}
+
+static int getMinutes(void) {
+    int minutes = -1;
+
+    bool isUserInputValid = false;
+    while (!isUserInputValid) {
+        printf("%s", "Enter minutes (0-59): ");
+        const int scanfResult = scanf("%d", &minutes);
+
+        isUserInputValid =
+            scanfResult == 1 &&
+            minutes >= 0 && minutes <= 59;
+
+        if (!isUserInputValid) {
+            fflush(stdin);
+            puts("You've entered incorrect minutes. Try again.");
+        }
+    };
+
+    return minutes;
+}
+
+static int getSeconds(void) {
+    int seconds = -1;
+
+    bool isUserInputValid = false;
+    while (!isUserInputValid) {
+        printf("%s", "Enter seconds (0-59): ");
+        const int scanfResult = scanf("%d", &seconds);
+
+        isUserInputValid =
+            scanfResult == 1 &&
+            seconds >= 0 && seconds <= 59;
+
+        if (!isUserInputValid) {
+            fflush(stdin);
+            puts("You've entered incorrect seconds. Try again.");
+        }
+    };
+
+    return seconds;
+}
+
+static long calculateSecondsSinceLastTwelve(
+    const int twelveHoursClockHours,
+    const int minutes,
+    const int seconds) {
+    int hours = (twelveHoursClockHours == 12) ? 0 : twelveHoursClockHours;
+
+    return hours * 3600 + minutes * 60 + seconds;
+}
+
+static long getSecondsSinceLastTwelve(void) {
+    const int twelveHoursClockHours = getTwelveHourClockHours();
+    const int minutes = getMinutes();
+    const int seconds = getSeconds();
+
+    return calculateSecondsSinceLastTwelve(twelveHoursClockHours, minutes, seconds);
+}
 
 int main(void) {
+    const long firstTime = getSecondsSinceLastTwelve();
+
+    puts("");
+
+    const long secondTime = getSecondsSinceLastTwelve();
+
+    printf("\nAmount of time between two entered times is: %ld.\n", labs(firstTime - secondTime));
+
     return EXIT_SUCCESS;
 }
--- ch05/ex24/currency_conversion.c
+++ ch05/ex24/currency_conversion.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define USD_TO_YEN_COURSE 118.87
+#define USD_TO_EURO_COURSE 0.92
+
+#define NUMBER_OF_DOLLAR_AMOUNTS 10
+
+static const double dollarAmounts[NUMBER_OF_DOLLAR_AMOUNTS] = {
+    1.3, 7.4, 8.1, 4.4, 5.6, 1.8, 44.5, 8.23, 2.38, 173.26
+};
+
+static double toYen(const double usDollars) {
+    return usDollars * USD_TO_YEN_COURSE;
+}
+
+static double toEuro(const double usDollars) {
+    return usDollars * USD_TO_EURO_COURSE;
+}
+
 int main(void) {
+    printf("%-7s   %-10s   %-7s\n", "Dollars", "Yens", "Euros");
+
+    for (size_t i = 0; i < NUMBER_OF_DOLLAR_AMOUNTS; ++i) {
+        const double dollarAmount = dollarAmounts[i];
+        const double yenAmount = toYen(dollarAmount);
+        const double euroAmount = toEuro(dollarAmount);
+
+        printf(
+            "%-7.2lf   %-10.2lf   %-7.2lf\n",
+            dollarAmount,
+            yenAmount,
+            euroAmount);
+    }
+
     return EXIT_SUCCESS;
 }
